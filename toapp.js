79e73c872b6f56b50dc28748c2ebb420fcd33659
Michael <real.michael.real@gmail.com>
2020-02-05 21:44:32 -0500

    v0

---
+++ toapp.js
@@ -0,0 +1,4 @@
+document.writeln('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
+document.writeln('<meta name="apple-mobile-web-app-capable" content="yes" />')
+document.writeln('<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>')
+document.writeln("<script type='text/javascript' charset='utf-8'>window.addEventListener('load', function(e) {setTimeout(function() { window.scrollTo(0, 1); }, 1);}, false);</script>")
